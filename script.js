9ea62b9773a8eda336b97d496ac8348097d7fa1a
Pedro Mascimo <pedromascimo@hotmail.com>
2022-01-10 13:26:43 -0300

    correção ortográfica

--- script.js
+++ script.js
@@ -12,9 +12,7 @@ function toggleMenu(){
 }
 
 
-
 //Desliga menu caso ativo fora do mobile
-
 window.addEventListener('resize', function () {
 
     var tela = window.innerWidth
@@ -24,32 +22,3 @@ window.addEventListener('resize', function () {
         menu.classList.remove("activer");
     }
 })
-
-
-
-
-
-window.onload = function (){
-
-// Links Internos
-var menuitens = document.querySelectorAll("#body a[href^='#']");
-
-menuitens.forEach(item => {
-    item.addEventListener("click", scrollId);
-})
-
- }
-
-function scrollId(event) {
-
-    event.preventDefault();
-    const element = event.target;
-    const id = element.getAttribute('href');  // ta pegando a tag #css que coloquei de cada elemento
-
-    const posicao = document.querySelector(id).offsetTop;   //especificando o local onde esta o link para saber distancia do topo
-
-    window.scroll({
-        top: posicao - 120,
-        behavior: "smooth",
-    });
-}
